93563c3788a55825d432032d7450b36cea23c3b5
david musembi <dmusembi702@gmail.com>
2026-01-07 01:34:04 +0300

    edited the codes

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -7,39 +7,26 @@ const products =[
   "Headphones", 
   "Monitor" ]
 
-function logFirstProduct(products) {
-  if (products.length > 0) {
-    console.log(products[0]);
-  } else {
-    console.log("No products available");
-  }
+function logFirstProduct() {
+  
+  console.log(products[0])
 }
 
-
-logFirstProduct(products);
 
 // adding addProduct function
-function addProduct(products, productName) {
+function addProduct(productName) {
   products.push(productName)
 }
-addProduct(products, "Keyboard");
-
-console.log(products);
 
 // adding updateProductName function
-function updateProductName(products, index, newName) {
+function updateProductName(index, newName) {
   products[index] = newName;
 }
-updateProductName(products, 1, "Smartphone");
-
-console.log(products);
 
 // adding remove function
 function removeLastProduct(products) {
-  products.pop();
+  products.pop()
 }
-removeLastProduct(products);
-console.log(products);
 
 // Export the necessary parts for testing
 module.exports = {
