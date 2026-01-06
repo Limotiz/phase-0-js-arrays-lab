c02f8154bcb888e0fa6c2c9e63df18b2040fe781
david musembi <dmusembi702@gmail.com>
2026-01-07 01:18:21 +0300

    made some changes on inventoryManagement

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -5,28 +5,37 @@ const products =[
   "Laptop", 
   "Phone", 
   "Headphones", 
-  "Monotor" ]
+  "Monitor" ]
 
 function logFirstProduct(products) {
   
   console.log(products[0])
 }
+
+logFirstProduct(products);
+
 // adding addProduct function
 function addProduct(products, productName) {
   products.push(productName)
 }
+addProduct(products, "Keyboard");
+
+console.log(products);
+
 // adding updateProductName function
 function updateProductName(products, index, newName) {
   products[index] = newName;
 }
+updateProductName(products, 1, "Smartphone");
 
+console.log(products);
+
+// adding remove function
 function removeLastProduct(products) {
   products.pop();
 }
-// adding remove function
-removeLastProduct(products, [2])
-
-console.log(products)
+removeLastProduct(products);
+console.log(products);
 
 // Export the necessary parts for testing
 module.exports = {
